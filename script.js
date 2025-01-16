9c646f3b7a180f46811ced31e4950a8b5cea2fcb
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-17 00:22:34 +0530

    added files

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello")
+console.log("hello");
+document.querySelector('.cross').style.display = 'none';
+document.querySelectorAll('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('siderbargo');
+
+})
